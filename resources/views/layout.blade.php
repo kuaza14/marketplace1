628c5c8021212130967137ded67d129fe8083133
kuaza14 <kuaza14321994@gmail.com>
2025-06-05 22:43:50 -0500

    prueba de migracion

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -157,10 +157,10 @@
             </div>
             <div class="nav-item dropdown">
               <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown" aria-label="Open user menu">
-                <span class="avatar avatar-sm" style="background-image: url(./static/jdx.jpg)"></span>
+                <span class="avatar avatar-sm" style="background-image: url(./static/tanjiro.jpg)"></span>
                 <div class="d-none d-xl-block ps-2">
-                  <div>Juan D.</div>
-                  <div class="mt-1 small text-secondary">Programador chiquito</div>
+                  <div>Edson C.</div>
+                  <div class="mt-1 small text-secondary">Programador peque</div>
                 </div>
               </a>
               <div class="dropdown-menu dropdown-menu-end dropdown-menu-arrow" data-bs-theme="light">
